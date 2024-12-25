97ed723d4df553d7748848f2e854988ba6431f2e
Shree Takalkar <shreetakalkar087@gmail.com>
2024-12-25 11:49:15 +0530

    push all files

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-if(process.env.NODE_ENV!="production"){
+if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
 
 console.log(process.env.SECRET);
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -14,21 +15,14 @@ const Review = require("./models/review.js");
 const session = require("express-session");
 const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
-const password=require("passport");
-const LocalStrategy=require("passport-local");
-const User=require("./models/user.js");
 const passport = require("passport");
-const userRouter =require("./routes/user.js");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+const userRouter = require("./routes/user.js");
 const listingsRouter = require("./routes/listing.js");
 const reviewsRouter = require("./routes/review.js");
-const { error } = require('console');
-const { render } = require('ejs');
 
-
-
-
-const dburl=process.env.ATLASDB_URL;
-
+const dburl = process.env.ATLASDB_URL;
 
 async function main() {
     try {
@@ -40,40 +34,39 @@ async function main() {
 }
 
 main();
+
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-app.set("views", path.join(__dirname, "views"));
-app.engine("ejs", ejsmate);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', ejsmate);
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.static('public'));
 
-const store=MongoStore.create({
-    mongoUrl:dburl,
-    crypto:{
-        secret:process.env.SECRET,
+const store = MongoStore.create({
+    mongoUrl: dburl,
+    crypto: {
+        secret: process.env.SECRET,
     },
-    touchAfter:24 * 3600,
+    touchAfter: 24 * 3600, // 24 hours
 });
 
-store.on("error",()=>{
-    console.log("ERROR IN MONGO SESSION STORE",err);
-})
+store.on("error", (err) => {
+    console.log("ERROR IN MONGO SESSION STORE:", err);
+});
 
 const sessionOptions = {
     store,
-    secret:process.env.SECRET,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
-        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge: 7 * 24 * 60 * 60 * 1000,
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000, // 1 week
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
         httpOnly: true,
+        secure: process.env.NODE_ENV === "production", // set secure cookies in production
     },
 };
-
-
 
 app.use(session(sessionOptions));
 app.use(flash());
@@ -84,33 +77,33 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
-  res.locals.currUser=req.user;
+    res.locals.currUser = req.user;
     next();
 });
 
 app.use("/listings", listingsRouter);
 app.use("/listings/:id/reviews", reviewsRouter);
-app.use("/",userRouter);
+app.use("/", userRouter);
 
 app.get("/", (req, res) => {
-    res.render("listings", "home.ejs");
+    res.render("home", { title: "Home Page" });
 });
 
+// Catch-all route for 404 errors
 app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page not Found"));
 });
 
-
+// Global error handler
 app.use((err, req, res, next) => {
     const { statusCode = 500, message = "Something went wrong" } = err;
     const stack = process.env.NODE_ENV !== "production" ? err.stack : null;
-    res.status(statusCode).render("error.ejs", { message, stack });
+    console.error(err); // Log error to console or external service
+    res.status(statusCode).render("error", { message, stack });
 });
-
 
 app.listen(8080, () => {
     console.log("Server is listening on port 8080");
