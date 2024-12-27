86d17788019b1bc1ed103e67eec53619a5ba1d00
Shree Takalkar <shreetakalkar087@gmail.com>
2024-12-27 14:26:22 +0530

    react

---
+++ client/src/HomePage.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to VentureVista - Tourist Places</h1>
+      <p>Explore amazing tourist destinations!</p>
+    </div>
+  );
+};
+
+export default HomePage;
