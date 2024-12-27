7c092567a164d0c491558b7abae3eb698c89d855
Shree Takalkar <shreetakalkar087@gmail.com>
2024-12-27 12:51:43 +0530

    home page changes

--- routes/user.js
+++ routes/user.js
@@ -7,7 +7,7 @@ const { saveRedirectUrl } = require("../middleware.js");
 const userController=require("../controllers/users.js");
 
 
-router.route("/")
+router.route("/home")
 .get(userController.renderhomepage);
 
 router.route("/signup")
