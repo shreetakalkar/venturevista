1a81b054aebf779f1cb98b9de202e1f0ef67aa97
Shree Takalkar <shreetakalkar087@gmail.com>
2024-12-27 17:46:17 +0530

    react

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000, // You can set the Vite development server to run on a specific port (e.g., 3000)
-    proxy: {
-      '/api': 'http://localhost:8080', // This can be useful if you need to proxy API requests to your backend
-    },
-  },
-});
+})
