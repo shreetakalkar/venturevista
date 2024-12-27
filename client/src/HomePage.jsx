53a951d378802c5b4b0ac212dd0e7e60fc7e99b6
Shree Takalkar <shreetakalkar087@gmail.com>
2024-12-27 17:06:51 +0530

    react

---
+++ client/src/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to the HomePage!</h1>
+      <p>This is your home page content rendered by React.</p>
+    </div>
+  );
+}
+
+export default HomePage;
