b1b802edcb8953b4e5df6916a40cb14d7a587eb4
Shree Takalkar <shreetakalkar087@gmail.com>
2024-12-27 17:10:44 +0530

    react

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/listings': 'http://localhost:8080', // Only proxy API requests to the backend
-      // other routes like /listings can also be added if needed
-    },
-    port: 3000, // React development server will run on port 3000
-  },
-});
+  plugins: [react()],
+})
